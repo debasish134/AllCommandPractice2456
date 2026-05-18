93e25bd52d755b04abc5656bd43d62f5205e3591
debasish134 <pdebasish318@gmail.com>
2026-05-18 12:06:18 +0530

    Added login Automation test classes

---
+++ src/test/java/com/Pracatice/Git/AllCommandPractice/FirstFile.java
@@ -0,0 +1,7 @@
+package com.Pracatice.Git.AllCommandPractice;
+
+public class FirstFile {
+  public static void main(String[] args) {
+	System.out.println("I am workinng as a Senior Automation tester");
+}
+}
